38206e7aeac011c5d265459db10b803833fc9afb
vartularora45 <138357352+vartularora45@users.noreply.github.com>
2025-10-14 16:43:52 +0530

    b

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors(
     {
-        origin: ["http://localhost:5173","https://ayursutra-alpha.vercel.app/"],
+        origin: ["http://localhost:5173","https://ayursutra-alpha.vercel.app"],
         credentials: true,
 
     }
